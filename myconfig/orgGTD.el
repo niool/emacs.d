d003e8b41e59abffcbeea6361adc37f543a9dd2c
niool <niool@hotmail.com>
2018-07-08 20:54:48 +0800

    Tune Time report format.

--- myconfig/orgGTD.el
+++ myconfig/orgGTD.el
@@ -322,6 +322,19 @@
 ;;
 (setq org-agenda-cmp-user-defined 'bh/agenda-sort)
 
+;; ============================================================================
+;; Time Report
+
+;; Agenda clock report parameters
+(setq org-agenda-clockreport-parameter-plist
+      (quote (:link t :maxlevel 5 :fileskip0 t :compact t :narrow 80)))
+; Set default column view headings: Task Effort Clock_Summary
+(setq org-columns-default-format "%80ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM")
+; global Effort estimate values
+; global STYLE property values for completion
+(setq org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+                                    ("STYLE_ALL" . "habit"))))
+
 
 ;; ============================================================================
 ;; Archiving Tasks
